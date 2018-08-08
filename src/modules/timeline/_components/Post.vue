075c428c8a3ae3b22fb749cb0743dc33a9e81ae9
Felix404 <felix.schmidthh@gmail.com>
2018-08-08 10:06:43 +0200

    module example

---
+++ src/modules/timeline/_components/Post.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <div class="post-container">
+    <div>{{ post.owner }}</div>
+    <div>{{ post.content }}</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name:'Post',
+  props: ['post']
+}
+</script>
+
+<style>
+  .post-container {
+    height: 350px;
+  }
+</style>
